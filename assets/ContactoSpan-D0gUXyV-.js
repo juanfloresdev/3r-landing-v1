57ce7a12257a1191afc392bc608761d3ade71e6b
Juan Francisco Flores Padilla <58011829+juanfloresdev@users.noreply.github.com>
2024-10-18 16:22:33 -0600

    Updates

--- assets/ContactoSpan-D0gUXyV-.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as n}from"./index-BUw-oT9-.js";const a={setup(){return{}}},o={style:{"text-align":"center"}};function c(s,r,p,_,u,d){return n(),e("h3",o," Mandanos un mensaje para contactarte. ")}const i=t(a,[["render",c]]);export{i as default};
